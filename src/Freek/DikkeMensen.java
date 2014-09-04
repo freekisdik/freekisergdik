5e86de439fa55cb9a0a3fab160bef036a705100d
Freekisdik <spam172@live.nl>
2014-09-04 10:15:16 +0200

    First project

---
+++ src/Freek/DikkeMensen.java
@@ -0,0 +1,24 @@
+package Freek;
+
+public class DikkeMensen {
+	
+	private String blackoe;
+	private String dikzak;
+	private String jeMoedersString;
+	
+	public DikkeMensen(){
+		
+	}
+	public setBlackoe(){
+		
+		blackoe = "geel"
+				+ "rood";
+		
+	}
+	
+	public String getBlackoe(){
+		
+		return blackoe;
+	}
+
+}
